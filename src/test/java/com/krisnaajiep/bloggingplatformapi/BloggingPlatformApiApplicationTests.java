bf3c6a59feafd3457faaca99d1426bc255ef2a39
krisnaajiep <krisnaajiep@gmail.com>
2025-05-05 16:22:06 +0700

    first commit

---
+++ src/test/java/com/krisnaajiep/bloggingplatformapi/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krisnaajiep.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
